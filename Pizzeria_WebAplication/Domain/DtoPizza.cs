d0d0faba42ef75c227f654a14ffe695cdbe27f24
MARL9108 <manuel910814@gmail.com>
2018-11-06 11:52:07 +0100

    DtoPizza

---
+++ Pizzeria_WebAplication/Domain/DtoPizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class DtoPizza
+    {
+        [Key]
+        public string Name { get; set; }
+        [Required]
+        public byte[] Picture { get; set; }
+        [Required]
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
